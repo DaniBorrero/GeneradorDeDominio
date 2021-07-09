53857c8674f40a99304c67d96afc94fabcae0ea2
DaniBorrero <borrerodaniel89@gmail.com>
2021-07-09 00:43:21 +0000

    Finish

--- src/app.js
+++ src/app.js
@@ -8,4 +8,35 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+  var exte = [".com", ".net", ".us", ".io"];
+
+  for (let index = 0; index < pronoun.length; index++) {
+    let element = pronoun[index];
+    //console.log(element);
+    for (let i = 0; i < adj.length; i++) {
+      let adjetivo = adj[i];
+      //console.log(x);
+      for (let j = 0; j < noun.length; j++) {
+        let sustantivo = noun[j];
+        for (let z = 0; z < exte.length; z++) {
+          let extension = exte[z];
+
+          console.log(element, adjetivo, sustantivo, extension);
+
+          let add = document.createElement("p");
+          const node = document.createTextNode(
+            element + adjetivo + sustantivo + extension
+          );
+          add.appendChild(node);
+
+          const div = document.getElementById("test");
+          div.appendChild(add);
+        }
+      }
+    }
+  }
 };
